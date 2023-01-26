ec62953d2b0f6cfd94895226593ad7a3a96c412c
codeDaiQin <1046572917@qq.com>
2023-01-26 19:56:33 +0800

    fix: 🐴 plugin

--- src/node/plugin.ts
+++ src/node/plugin.ts
@@ -92,7 +92,7 @@ export const pluginSvgr = (
     async transform(code, id) {
       if (!id.endsWith('.svg')) return code
 
-      const svgrTransform = await (await import('@svgr/core')).transform
+      const svgrTransform = (await import('@svgr/core')).transform
       const esbuild = await import('esbuild')
       const svg = await fs.promises.readFile(id, 'utf8')
       const svgrResult = await svgrTransform(
